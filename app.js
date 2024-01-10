b69dc0b38e78cab7d189bf0fd8d11910a1a8d8e2
markmarin500 <156245145+markmarin500@users.noreply.github.com>
2024-01-10 23:46:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+
+
+const backendURL = 'https://www.hb-studios.com';
+
+function pingBackend() {
+  axios.get(backendURL)
+    .then(response => {
+      console.log('Ping gönderildi, backend aktif!');
+    })
+    .catch(error => {
+      console.error('Ping gönderme hatası:', error);
+    });
+}
+
+
+const pingInterval = setInterval(pingBackend, 1000);
+const pingInterval2 = setInterval(pingBackend, 1000);
+const pingInterval3 = setInterval(pingBackend, 1000);
+//const pingInterval4= setInterval(pingBackend, 1);
+
+
+
+
+
+
+
+pingBackend()
+
+
+
